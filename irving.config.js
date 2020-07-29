0394d361ea972bdf72924b141ee1392fd3d64569
James Burke <james.walter.burke@gmail.com>
2020-07-29 10:59:11 -0400

    Remove unnecessary files, focusing on bare-minimum

--- irving.config.js
+++ irving.config.js
@@ -1,10 +1,5 @@
-import styledComponentsConfig from '@irvingjs/styled';
-
 const config = {
   name: 'create-irving-app',
-  packages: [
-    styledComponentsConfig,
-  ],
 };
 
 export default config;
