c10d8b683aa97bc5964eed34e42a55ce31c6adf1
Gabriel Luethje <gabriel@alley.co>
2020-11-17 10:27:21 -0700

    Update component map to support latest Irving

--- componentMap.js
+++ componentMap.js
@@ -1,5 +1,10 @@
-import ComponentMap from '@irvingjs/styled-components';
+import createThemedComponentMap from '@irvingjs/styled-components/componentMap';
+import { componentMap as WordPressComponentMap } from '@irvingjs/wordpress';
+import { IntegrationsManager } from '@irvingjs/integrations';
+import themes from './themes';
 
 export default {
-  ...ComponentMap({}),
+  ...createThemedComponentMap(themes),
+  ...WordPressComponentMap,
+  'irving/integrations': IntegrationsManager,
 };
